d56ef24f767564b87397ce42681ece301b936951
Gloria Montoya <gmontoya2021@gmail.com>
2026-03-14 10:50:47 -0500

    feat: Creo la rama develop y a partir de ella la rama feature/project-bootstrap con su mensaje de bienvenida

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**************************************************");
+        Console.WriteLine("* Bienvenido al Sistema de Gestión de Biblioteca *");
+        Console.WriteLine("***************************************************");
+        Console.WriteLine();
+        Console.WriteLine("Aquí tendrás las opciones para:");
+        Console.WriteLine("- Gestionar Libros");
+        Console.WriteLine("- Gestionar Usuarios");
+        Console.WriteLine("- Gestionar Préstamos");
+        Console.WriteLine("- Realizar Búsquedas");
+        Console.WriteLine();
+        Console.WriteLine("***************************************************");
+    }
+}
