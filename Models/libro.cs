b6fc13f6b07c4065f05edee2fa46ea040577c3b3
dj107-il <diegoballes4@gmail.com>
2026-03-19 23:07:21 -0500

    feat: actualizar clase Libro

--- Models/libro.cs
+++ Models/libro.cs
@@ -5,9 +5,12 @@ namespace libreria_la_gloria.Models
     public class Libro
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Autor { get; set; }
+
+        public string Titulo { get; set; } = "";
+        public string Autor { get; set; } = "";
+
         public int Anio { get; set; }
+
         public bool Disponible { get; set; }
 
         public Libro()
